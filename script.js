a8b2cf91a4375149a60c6a79ce3c041ef929885c
pang_loves_molamola <72025305+sgissweet@users.noreply.github.com>
2024-09-13 06:15:38 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function go_home() {
+    window.location.href = 'home.html';
+}
+
+function go_purple_cv() {
+    window.location.href = 'purple_cv.html';
+}
+
+function go_yellow_cv() {
+    window.location.href = 'yellow_cv.html';
+}
